72dd4b58764cce86a2e92ee1ddb8a5727900a0c9
DurgaPrashad <120201162+DurgaPrashad@users.noreply.github.com>
2023-03-18 15:34:32 +0530

    Create README.md.c

---
+++ README.md.c
@@ -0,0 +1,25 @@
+# sudent-managment-system
+c++ project
+Student Management System...
+
+This is a C++ project for managing student data.
+--------------------------------------------------------
+Installation....
+
+Clone the repository to your local machine.
+Compile the source code using a C++ compiler.
+Run the executable file.
+
+---------------------------
+Usage
+The program allows you to perform the following tasks:
+
+Add a new student record.
+View an existing student record.
+Update an existing student record.
+Delete an existing student record.
+Display all student records.
+To choose a task, enter the corresponding number at the main menu.
+-----------------------------------------------------------------------------
+Contributing
+Contributions are welcome! If you have any suggestions or bug reports, please open an issue or submit a pull request.
